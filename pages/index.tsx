1ba31f8ae676e3df785897556454269dad4006f1
Annaati <Abdi17fatah@gmail.com>
2022-05-18 03:18:43 +0300

    Header frontend Design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import Header from '../components/Header'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div >Welcome to Coin Market Cap Clone</div>
+    <div className='min-h-screen'>
+      <Header/>
+    </div>
   )
 }
 
